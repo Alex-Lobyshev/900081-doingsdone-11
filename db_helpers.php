ee876e8f5af0a8dec7c403c2343a8ba11d8eec08
Alex Lobyshev <lobyshev.alexandr@yandex.ru>
2019-11-19 20:20:46 +0300

    Вынес подключение к базе в функцию и подправил данные для подключения

--- db_helpers.php
+++ db_helpers.php
@@ -5,15 +5,15 @@ $db_data = $db_connect;
 
 function db_connection($db_data){
 
-    $link = mysqli_connect($db_data['host'], $db_data['user2'], $db_data['password'], $db_data['database']);
+    $link = mysqli_connect($db_data['host'], $db_data['user'], $db_data['password'], $db_data['database']);
     mysqli_set_charset($link, 'utf8');
 
 
     if (!$link) {
         $error = mysqli_connect_error();
         print ('Ошибка MySQL' . $error);
-        show_error($error);
     } else {
+        print_r($link);
         return $link;
     }
 }
